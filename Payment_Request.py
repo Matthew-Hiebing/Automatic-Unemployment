2aac46ade0ec1b373217609e14a1b671198161a8
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-07 09:30:36 -0600

    Import Work Search xlsx file from Excel
    Imports data from Excel sheet.

---
+++ Payment_Request.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_excel (r'C:\Users\Matt\Dropbox\1 Job Search Related Items\Daily Work Log.xlsx', sheet_name='TWC Work Search Log')
+
+print(df)
