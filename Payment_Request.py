2a12aaf3c98bc11a2487a60faa3cb30efbe3e427
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-15 10:30:09 -0600

    Divided Search Activity Into Two Weeks
    Prior code summed the work searches over two weeks of time.  Unemployment asks for work searches per week.  Search code was divided up so that it can sum the amount of work searches given two dates.  Additionally, instead of pulling a start and end date for each week I used the datetime module to add 7 days to the initial week start day.

--- Payment_Request.py
+++ Payment_Request.py
@@ -2,47 +2,48 @@ import pandas as pd
 import numpy as np
 import time
 import yaml
+import datetime
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 
 #-----------------------------------------------------------------------------#
 
 # Log into Texas Workforce Commission website.
-driver = webdriver.Firefox(
-    executable_path=
-    r'D:\Users\Matt\Documents\GitHub\Executable_Files\geckodriver.exe'
-)
+# driver = webdriver.Firefox(
+#     executable_path=
+#     r'D:\Users\Matt\Documents\GitHub\Executable_Files\geckodriver.exe'
+# )
 
-conf = yaml.load(open(r'D:\Users\Matt\Documents\GitHub\YML_Files\Unemployment_Login_Credentials.yml')
-)
+# conf = yaml.load(open(r'D:\Users\Matt\Documents\GitHub\YML_Files\Unemployment_Credentials.yml'))
 
-my_username = conf['Unemployment']['username']
-my_password = conf['Unemployment']['password']
+# my_username = conf['Unemployment']['username']
+# my_password = conf['Unemployment']['password']
 
-def login(url,usernameID,passwordID,login_button):
-    driver.get(url)
-    time.sleep(3)
-    driver.find_element_by_id(usernameID).send_keys(my_username)
-    driver.find_element_by_id(passwordID).send_keys(my_password)
-    driver.find_element_by_name(login_button).click()
+# def login(url,usernameID,passwordID,login_button):
+#     driver.get(url)
+#     time.sleep(3)
+#     driver.find_element_by_id(usernameID).send_keys(my_username)
+#     driver.find_element_by_id(passwordID).send_keys(my_password)
+#     driver.find_element_by_name(login_button).click()
 
-login(
-    'https://login.apps.twc.state.tx.us/UBS/security/logon.do',
-    'field.label.username',
-    'field.label.password',
-    'method:logon'
-)
+# login(
+#     'https://login.apps.twc.state.tx.us/UBS/security/logon.do',
+#     'field.label.username',
+#     'field.label.password',
+#     'method:logon'
+# )
 
-def payment_request(payment_request):
-    driver.find_element_by_xpath(payment_request).click()
-    if (driver.find_element_by_class_name('page-name').text) == 'Early Filing':
-        print('Try to file again on:',
-        driver.find_element_by_class_name('page-block').text[162:180])
-        driver.quit()
+# def payment_request(payment_request):
+#     driver.find_element_by_xpath(payment_request).click()
+#     if (driver.find_element_by_class_name('page-name').text) == 'Early Filing':
+#         print('Try to file again on:',
+#         driver.find_element_by_class_name('page-block').text[162:180])
+#         driver.quit()
 
-payment_request(
-    '/html/body/div/div[3]/div[1]/div/ul[1]/li[4]/a',
-)
+# payment_request(
+#     '/html/body/div/div[3]/div[1]/div/ul[1]/li[4]/a',
+# )
 
 #-----------------------------------------------------------------------------#
 # Read excel file and sum work searches between two dates.
@@ -53,18 +54,33 @@ data = pd.read_excel(
 
 df = pd.DataFrame(data, columns= ['Date of Activity', 'Work Search Count'])
 
+week1_start_date = '11-01-2020'
 
+date_1 = datetime.datetime.strptime(week1_start_date, r'%m-%d-%Y')
+end_date1 = date_1 + datetime.timedelta(days=7)
 
-start_date = '10-30-2020' # Have Sellenium pulled date.
-end_date = '11-12-2020' # Have Sellenium pulled date.
+after_week1 = (df[df['Date of Activity'] >= date_1])
+before_end_date = (df[df['Date of Activity'] <= end_date1])
+between_two_dates = (df[(df['Date of Activity']>=date_1) &
+(df['Date of Activity']<=end_date1)])
 
-after_start_date = (df[df['Date of Activity'] >= start_date])
-before_end_date = (df[df['Date of Activity'] <= end_date])
-between_two_dates = (df[(df['Date of Activity']>=start_date) &
-                    (df['Date of Activity']<=end_date)])
+week1_work_searches = between_two_dates.sum()
+print(week1_work_searches)
 
-total_work_searches = between_two_dates.sum()
+#--------Week 1--------------------#
 
+week2_start_date = '11-09-2020'
+
+date_2 = datetime.datetime.strptime(week2_start_date, r'%m-%d-%Y')
+end_date2 = date_2 + datetime.timedelta(days=7)
+
+after_week2 = (df[df['Date of Activity'] >= date_2])
+before_end_date = (df[df['Date of Activity'] <= end_date2])
+between_two_dates = (df[(df['Date of Activity']>=date_2) &
+(df['Date of Activity']<=end_date2)])
+
+week2_work_searches = between_two_dates.sum()
+print(week2_work_searches)
 
 
 
