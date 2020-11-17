df18d89fbb26edaf40ee1de5e685279fb85fffde
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-17 10:11:57 -0600

    Added Code Comments
    Added code comments to make code more readable for others.

--- Payment_Request.py
+++ Payment_Request.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 #-----------------------------------------------------------------------------#
-# Log into Texas Workforce Commission website.
+# LOG INTO TWC WEBSITE AND BEGIN PAYMENT REQUEST
 driver = webdriver.Firefox(
     executable_path=
     r'D:\Users\Matt\Documents\GitHub\Executable_Files\geckodriver.exe'
@@ -34,47 +34,41 @@ login(
 )
 
 def payment_request():
+    # CHECKS IF ITS TOO EARLY TO FILE. IF IT IS, THE SCRIPT STOPS
     driver.find_element_by_xpath('/html/body/div/div[3]/div[1]/div/ul[1]/li[4]/a').click()
     if (driver.find_element_by_class_name('page-name').text) == 'Early Filing':
-        print('Try to file again on:',
-        driver.find_element_by_class_name('page-block').text[162:180])
         driver.quit()
+    # IF ITS NOT TOO EARLY TO FILE THE PAYMENT REQUEST CONTINUES
     else:
+        # PULLS CLAIM WEEK DATES FROM TWC
         claim_week_pull = driver.find_element_by_class_name('page-header-row-value-1col').text[0:12]
         claim_week_convert_to_str = claim_week_pull.replace(',','')
         date_1 = datetime.datetime.strptime(claim_week_convert_to_str,r'%b %d %Y')
 
         #---------------------------------------------------------------------#
-        # ADD UP WORK SEARCHES FOR WEEK 1
-        data = pd.read_excel(
-            r'C:\Users\Matt\Dropbox\1 Job Search Related Items\Daily Work Log.xlsx',
-            sheet_name='TWC Work Search Log'
-        )
+        # CREATE DATAFRAME FROM LOCAL EXCEL FILE AND READS TWO COLUMNS OF DATA
+        data = pd.read_excel(r'C:\Users\Matt\Dropbox\1 Job Search Related Items\Daily Work Log.xlsx', sheet_name='TWC Work Search Log')
         df = pd.DataFrame(data, columns= ['Date of Activity', 'Work Search Count'])
 
+        # ADD UP WORK SEARCHES FOR WEEK 1
         end_date1 = date_1 + datetime.timedelta(days=6)
-
         between_two_dates1 = (df[(df['Date of Activity']>=date_1) & (df['Date of Activity']<=end_date1)])
-
         week1_work_searches = between_two_dates1.sum()
-        print(date_1)
-        print(end_date1)
-        print(week1_work_searches)
 
         # ADD UP WORK SEARCHES FOR WEEK 2
         date_2 = date_1 + datetime.timedelta(days=7)
         end_date2 = date_2 + datetime.timedelta(days=6)
-
-        between_two_dates2 = (df[(df['Date of Activity']>=date_2) &
-        (df['Date of Activity']<=end_date2)])
-
+        between_two_dates2 = (df[(df['Date of Activity']>=date_2) & (df['Date of Activity']<=end_date2)])
         week2_work_searches = between_two_dates2.sum()
-        print(date_2)
-        print(end_date2)
-        print(week2_work_searches)
 
         #---------------------------------------------------------------------#
         #CONTINUE PAYMENT REQUEST
+        '''
+        This series of button clicks goes through the website pages, fills in
+        the radio buttons and enters the work searches you've completed into
+        the required fields when they come up.  After this is complete the
+        script is ended and the browser window closes.
+        '''
         driver.find_element_by_id('addressChangeAnswer-radio.label.no').click()
         driver.find_element_by_name('method:submit').click()
         time.sleep(5)
@@ -100,7 +94,7 @@ def payment_request():
         driver.find_element_by_name('method:submit').click()
         driver.quit()
 
-
+# STARTS PAYMENT REQUEST FUNCTION
 payment_request()
 
 
